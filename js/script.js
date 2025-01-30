2fbfa72d36eb0ae507770b28875cf5b5c024175b
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-01-30 06:37:04 +0100

    gættespil

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
-"use strict";
+`use strict`;
 
+let gameRunning = true;
+
+{
+if (gameRunning === false) {
+    alert(`Du kan ikke spille lige nu`);
+} else {
+    let userNumber = parseInt(window.prompt(`Skriv et number mellem 1 & 100`));
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    let attempts = 0;
+    let maxAttempts = 1;
+
+    while (userNumber !== randomNumber && attempts < maxAttempts) {
+        if (userNumber < randomNumber) {
+            userNumber = parseInt(prompt(`Nummeret er HØJERE! Prøv igen!`));
+        } else {
+            userNumber = parseInt(prompt(`Nummeret er LAVERE! Prøv igen!`));
+        }
+        attempts++;
+    }
+
+    if (userNumber === randomNumber) {
+        alert(`Tillykke! Du gættede tallet på ${attempts} forsøg!`);
+    } else {
+        alert(`Du tabte, det rigtige nummer var: ${randomNumber}`);
+    }
+}
+}
